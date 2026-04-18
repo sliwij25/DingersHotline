62321ae3b474ce6ca9ee675aac01882ea09b1c1a
Joe Sliwinski <sliwij25@gmail.com>
2026-04-17 20:03:34 -0500

    chore: delete dead OverseerAgent + clean __init__ exports

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,9 +1,6 @@
-from .bet_tracker import (BetTrackerAgent, save_pick_factors,
-                          factor_performance_report)
+from .bet_tracker import (save_pick_factors, factor_performance_report)
 from .predictor import Homer
-from .overseer import OverseerAgent
 from .backtester import run_backtest, backtest_report
 
-__all__ = ["BetTrackerAgent", "Homer", "OverseerAgent",
-           "run_backtest", "backtest_report",
+__all__ = ["Homer", "run_backtest", "backtest_report",
            "save_pick_factors", "factor_performance_report"]
